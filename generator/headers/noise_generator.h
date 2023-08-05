a05b3699d74458ca01f7306132a30d1f60288990
TapekhaVA <tapehavladimir@gmail.com>
2023-08-05 12:45:14 +0300

    add generators

---
+++ generator/headers/noise_generator.h
@@ -0,0 +1,25 @@
+#ifndef DIGITAL_SIGNAL_PROCESSING_NOISE_GENERATOR_H
+#define DIGITAL_SIGNAL_PROCESSING_NOISE_GENERATOR_H
+
+#include <random>
+
+static const double k_noise_dispersion = 1e-6;
+
+/*! \class NoiseGenerator
+*   \brief Generate noise with normal distribution
+*/
+class NoiseGenerator
+{
+public:
+    //!
+    inline NoiseGenerator(): _rd(), _gen(_rd()), _distribution(0, k_noise_dispersion) {}
+    //!
+    inline double generate() { return _distribution(_gen); }
+
+private:
+    std::random_device _rd;
+    std::mt19937 _gen;
+    std::normal_distribution<double> _distribution;
+};
+
+#endif //DIGITAL_SIGNAL_PROCESSING_NOISE_GENERATOR_H
