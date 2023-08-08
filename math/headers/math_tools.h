89a6b94df307206564fac149803101f853643b8b
TapekhaVA <tapehavladimir@gmail.com>
2023-08-08 10:40:55 +0300

    fft first vesion

--- math/headers/math_tools.h
+++ math/headers/math_tools.h
@@ -2,11 +2,22 @@
 #define DIGITAL_SIGNAL_PROCESSING_MATH_TOOLS_H
 
 #include <vector>
+#include <cmath>
+#include <complex>
+
+using ftype = std::complex<double>;
+static const double pi = acos(-1);
 
 class MathTools
 {
 public:
     static double dispersion(const std::vector<double>& array);
+
+    static std::vector<ftype> fft(const std::vector<ftype>& values, ftype wn);
+private:
+    std::vector<ftype> evaluate(std::vector<double> values);
+
+    static std::vector<ftype> castToComplex(const std::vector<double>& array);
 };
 
 #endif //DIGITAL_SIGNAL_PROCESSING_MATH_TOOLS_H
--- math/src/math_tools.cpp
+++ math/src/math_tools.cpp
@@ -1,6 +1,7 @@
 #include <math_tools.h>
 
 #include <numeric>
+#include <cassert>
 #include <cmath>
 
 double MathTools::dispersion(const std::vector<double> &array)
@@ -17,4 +18,51 @@ double MathTools::dispersion(const std::vector<double> &array)
     dispersion /= size;
 
     return dispersion;
+}
+
+std::vector<ftype> MathTools::evaluate(std::vector<double> values)
+{
+    while(__builtin_popcount(static_cast<uint32_t>(values.size())) != 1)
+    {
+        values.push_back(0);
+    }
+
+    return fft(castToComplex(values), std::polar(1., 2 * pi / static_cast<double>(values.size())));
+}
+
+std::vector<ftype> MathTools::fft(const std::vector<ftype> &values, ftype wn)
+{
+    size_t size = values.size();
+    assert(size == 1);
+
+    std::vector<ftype> result(size);
+
+    std::vector<ftype> odd, even;
+    for (size_t i = 0; i < size / 2; i++) {
+        even.push_back(values.at(2 * i));
+        odd.push_back(values.at(2 * i + 1));
+    }
+
+    fft(even, wn * wn);
+    fft(odd, wn * wn);
+
+    ftype w = 1;
+    for (size_t i = 0; i < size / 2; i++) {
+        result.at(i) = even.at(i) + w * odd.at(i);
+        result.at(i + size / 2) = even.at(i) - w * odd.at(i); //! w^(i+n/2) = -w^i
+        w *= wn;
+    }
+
+    return result;
+}
+
+std::vector<ftype> MathTools::castToComplex(const std::vector<double> &array)
+{
+    std::vector<ftype> result;
+    for (auto i : array)
+    {
+        result.emplace_back(i);
+    }
+
+    return result;
 }
