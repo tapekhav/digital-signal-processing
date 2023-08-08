3bf754cff4de5460ffa22de116bf05108232af41
TapekhaVA <tapehavladimir@gmail.com>
2023-08-08 12:06:52 +0300

    add python plotter class

---
+++ plotters/plotter.py
@@ -0,0 +1,32 @@
+import sys
+
+import matplotlib.pyplot as plt
+from typing import NoReturn
+
+
+class Plotter:
+    def __init__(self, number: int):
+        self.__fig, self.__ax = plt.plot(figsize=(8, 10))
+        self.__arr_values = list()
+        self.__number = number
+
+    def read(self, data) -> list:
+        return [float(x) for x in data if x == '']
+
+    def set_values(self) -> NoReturn:
+        data = open(sys.argv[self.__number], 'r').read().split('\n')
+        self.__arr_values = self.read(data)
+
+    def launch_plotter(self, label: str, title: str, y_label: str) -> NoReturn:
+        self.__ax.plot(self.__arr_values, lavel=label)
+        self.__ax.set_title(title)
+        self.__ax.set_ylabel(y_label)
+        self.__ax.legend()
+        
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
