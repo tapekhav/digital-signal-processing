e14476ad72db1cb270f98886d86595995c293582
TapekhaVA <tapehavladimir@gmail.com>
2023-08-10 16:06:37 +0300

    add mean value and root mean square methods

--- math/headers/math_tools.h
+++ math/headers/math_tools.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include <cmath>
 #include <complex>
+#include <numeric>
+#include <iterator>
 
 using ftype = std::complex<double>;
 static const double pi = acos(-1);
@@ -11,13 +13,17 @@ static const double pi = acos(-1);
 class MathTools
 {
 public:
+    static double meanValue(const std::vector<double>& values, long size);
+
     static double dispersion(const std::vector<double>& array);
 
+    static inline double rootMeanSquare(const std::vector<double>& array) { return std::sqrt(dispersion(array)); }
+
     static std::vector<ftype> fft(const std::vector<ftype>& values, ftype wn);
-private:
-    std::vector<ftype> evaluate(std::vector<double> values);
 
     static std::vector<ftype> castToComplex(const std::vector<double>& array);
+private:
+    std::vector<ftype> evaluate(std::vector<double> values);
 };
 
 #endif //DIGITAL_SIGNAL_PROCESSING_MATH_TOOLS_H
