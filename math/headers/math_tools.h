490ffd4e296b0aeffe0e65c2cd50aa2170a6fde4
TapekhaVA <tapehavladimir@gmail.com>
2023-08-11 12:05:38 +0300

    add iterativeFFT

--- math/headers/math_tools.h
+++ math/headers/math_tools.h
@@ -8,7 +8,6 @@
 #include <iterator>
 
 using ftype = std::complex<double>;
-static const double pi = acos(-1);
 
 class MathTools
 {
@@ -19,11 +18,13 @@ public:
 
     static inline double rootMeanSquare(const std::vector<double>& array) { return std::sqrt(dispersion(array)); }
 
-    static std::vector<ftype> fft(const std::vector<ftype>& values, ftype wn);
+    static std::vector<ftype> iterativeFFT(const std::vector<ftype>& values);
 
     static std::vector<ftype> castToComplex(const std::vector<double>& array);
 private:
-    std::vector<ftype> evaluate(std::vector<double> values);
+    static uint32_t bit_reverse(uint32_t num, int bits);
+
+    static void bit_reverse_copy(const std::vector<ftype>& values, std::vector<ftype>& A);
 };
 
 #endif //DIGITAL_SIGNAL_PROCESSING_MATH_TOOLS_H
--- math/src/math_tools.cpp
+++ math/src/math_tools.cpp
@@ -16,6 +16,7 @@ double MathTools::dispersion(const std::vector<double> &array)
     return dispersion;
 }
 
+/*
 std::vector<ftype> MathTools::evaluate(std::vector<double> values)
 {
     while(__builtin_popcount(static_cast<uint32_t>(values.size())) != 1)
@@ -23,7 +24,7 @@ std::vector<ftype> MathTools::evaluate(std::vector<double> values)
         values.push_back(0);
     }
 
-    return fft(castToComplex(values), std::polar(1., 2 * pi / static_cast<double>(values.size())));
+    return fft(castToComplex(values), std::polar(1., 2 * M_PI / static_cast<double>(values.size())));
 }
 
 std::vector<ftype> MathTools::fft(const std::vector<ftype> &values, ftype wn)
@@ -55,6 +56,64 @@ std::vector<ftype> MathTools::fft(const std::vector<ftype> &values, ftype wn)
     }
 
     return result;
+}
+*/
+
+uint32_t MathTools::bit_reverse(uint32_t num, int bits)
+{
+    uint32_t reversed = 0;
+    for (int i = 0; i < bits; ++i)
+    {
+        reversed = (reversed << 1) | (num & 1);
+        num >>= 1;
+    }
+
+    return reversed;
+}
+
+void MathTools::bit_reverse_copy(const std::vector<ftype>& values, std::vector<ftype>& A) {
+    int bits = 0;
+
+    while ((1 << bits) < values.size())
+    {
+        ++bits;
+    }
+
+    for (uint32_t k = 0; k < values.size(); ++k)
+    {
+        A[bit_reverse(k, bits)] = values[k];
+    }
+}
+
+std::vector<ftype> MathTools::iterativeFFT(const std::vector<ftype> &values)
+{
+    size_t n = values.size();
+    std::vector<ftype> A(n);
+
+    bit_reverse_copy(values, A);
+
+    for (int s = 1; s <= std::log2(n); s++)
+    {
+        double m = std::pow(2, s);
+        ftype omega_m = std::exp(ftype(0, -2.0 * M_PI / m));
+
+        auto q = static_cast<size_t>(m);
+        for (size_t k = 0; k < n; k += q)
+        {
+            ftype omega = 1.0;
+
+            for (size_t j = 0; j < q / 2; j++)
+            {
+                ftype t = omega * A[k + j + q / 2];
+                ftype u = A[k + j];
+                A[k + j] = u + t;
+                A[k + j + q / 2] = u - t;
+                omega *= omega_m;
+            }
+        }
+    }
+
+    return A;
 }
 
 std::vector<ftype> MathTools::castToComplex(const std::vector<double> &array)
