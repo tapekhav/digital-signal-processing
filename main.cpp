89b234af65ddf26dad9f4490d601249fad021d8a
TapekhaVA <tapehavladimir@gmail.com>
2023-08-11 17:32:28 +0300

    add test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
-
+#include <fft_autocorrelation_function.h>
+#include <math_tools.h>
 
 int main()
 {
+    std::vector<double> zv = {1.0, 2.0, 3.0, 4.0, 5.0};
+    MathTools::addToPowerOfTwo(zv);
+    FFTAutoCorrelation zxc(zv);
+    zxc.estimateSignals();
 
     return 0;
 }
